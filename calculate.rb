0fcd99ed603c9468c710c0d8e2fdb1d90ce9df2e
Dellshams <60371232+Dellshams@users.noreply.github.com>
2020-03-29 00:39:16 +0100

    Update calculate.rb
    First Ruby task

--- calculate.rb
+++ calculate.rb
@@ -5,7 +5,7 @@ def calculate (c)
 
 radius = 0.0
 area = 0.0
-print "Enter radius of circle: "
+print "Enter the radius of the circle: "
 radius = gets.to_f
 area = calculate(radius)
 puts "The area of the circle with radius #{radius} is #{area}." 
